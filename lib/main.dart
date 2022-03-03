a255e1f7adff0cc7bcc787f17c449df88c6aaa29
Hassan Ayoub Zafar <60160952+hassan-zafar@users.noreply.github.com>
2022-03-04 01:52:18 +0500

    changed name and added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:silend/Screens/transactions_screens.dart';
 import 'package:silend/Screens/welcome.dart';
 
 void main() {
@@ -13,18 +12,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Silend',
       theme: ThemeData(
-        brightness: Brightness.dark ,
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+        brightness: Brightness.dark,
         primarySwatch: Colors.blue,
       ),
       home: const WelcomescreenWidget(),
