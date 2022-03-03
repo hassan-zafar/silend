b80d91e4401513605137db8db21925cf3ae85245
Hassan Ayoub Zafar <60160952+hassan-zafar@users.noreply.github.com>
2022-03-04 02:28:37 +0500

    created request payment screen

---
+++ lib/Screens/request_payment.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class RequestPaymentPage extends StatefulWidget {
+  const RequestPaymentPage({Key? key}) : super(key: key);
+
+  @override
+  State<RequestPaymentPage> createState() => _RequestPaymentPageState();
+}
+
+class _RequestPaymentPageState extends State<RequestPaymentPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [Text('asd')],
+        ),
+      ),
+    );
+  }
+}
